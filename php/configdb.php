8cfc6001d58a32089c372de80153d8591a690fc2
SergioPoves-daw <spovescamison.guadalupe@alumnado.fundacionloyola.net>
2026-03-23 17:52:34 +0100

    Update configdb.php

--- php/configdb.php
+++ php/configdb.php
@@ -1,6 +1,6 @@
 <?php
-    define("SERVIDOR",'esvirgua.com');
-    define("USUARIO",'user1daw_20');
-    define("PASSWORD",'0V$G7Z}Tk9VD0-&C');
-    define("BBDD",'user1daw_BD1-20');
+    define("SERVIDOR",'...');
+    define("USUARIO",'...');
+    define("PASSWORD",'...');
+    define("BBDD",'...');
 ?>
