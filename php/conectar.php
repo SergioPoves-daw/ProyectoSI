1d1d6f2ecf06a9eaafe8215d4982972e529cc371
SergioPoves-daw <spovescamison.guadalupe@alumnado.fundacionloyola.net>
2026-03-24 18:32:56 +0100

    conectar y listado actualizados

--- php/conectar.php
+++ php/conectar.php
@@ -1,5 +1,5 @@
 <?php
-    function conectar(){
+  function conectar(){
     $conexion = new mysqli(SERVIDOR, USUARIO, PASSWORD, BBDD);
     $conexion->set_charset("utf8"); 
     return $conexion;
--- php/mostrarJesuitas.php
+++ php/mostrarJesuitas.php
@@ -7,22 +7,20 @@
     return $conexion;
   }
 
-  function mostrar_jesuitas() {
-    $conexion = conectar();
+  $conexion = conectar();
 
-    $sql = 'SELECT nombreJesuita FROM alumnos';
-    $resultado = $conexion->query($sql);
+  $sql = 'SELECT nombreJesuita FROM alumnos';
+  $resultado = $conexion->query($sql);
 
-    // Mostrar nombreJesuita tantos option como filas hayan
-    // fetch_array() devuelve el array unidimensional y TRUE/FALSE
-    // Cuando es final de puntero y no hay más filas, devuelve false
-    while ($fila = $resultado->fetch_array()) {
+  // Mostrar nombreJesuita tantos option como filas hayan
+  // fetch_array() devuelve el array unidimensional y TRUE/FALSE
+  // Cuando es final de puntero y no hay más filas, devuelve false
+  while ($fila = $resultado->fetch_array()) {
 
-    // Primero el option value con el valor del nombreJesuita, después el texto con el mismo valor y por ultimo </option>
-    echo '<option value="' . $fila["nombreJesuita"] . '">' . $fila["nombreJesuita"] . '</option>';
-    }
-    // Cuando no hay siguiente fila, devuelve false y acaba el bucle
-    
-    $conexion->close();
+  // Primero el option value con el valor del nombreJesuita, después el texto con el mismo valor y por ultimo </option>
+  echo '<option value="' . $fila["nombreJesuita"] . '">' . $fila["nombreJesuita"] . '</option>';
   }
+  // Cuando no hay siguiente fila, devuelve false y acaba el bucle
+  
+  $conexion->close();
 ?>
