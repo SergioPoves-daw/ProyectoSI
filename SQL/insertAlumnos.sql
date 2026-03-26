4689f796209177914dae6a04dec4ce17880433a5
SergioPoves-daw <spovescamison.guadalupe@alumnado.fundacionloyola.net>
2026-03-26 16:49:15 +0100

    Anterior INSERT alumnos eliminado (ahora isertAlumnosAntiguo)

--- SQL/insertAlumnos.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO alumnos (idAlumno, nombre, pwd, nombreJesuita, descripcionJesuita, imagenJesuita) VALUES
-	(1, 'Alumno 1', '1111', 'Jesuita 1', 'Primer jesuita', 'img1.jpg'),
-	(2, 'Alumno 2', '2222', 'Jesuita 2', 'Segundo jesuita', 'img2.jpg'),
-	(3, 'Alumno 3', '3333', 'Jesuita 3', 'Tercer jesuita', 'img3.jpg'),
-	(4, 'Alumno 4', '4444', 'Jesuita 4', 'Cuarto jesuita', 'img4.jpg'),
-	(5, 'Alumno 5', '5555', 'Jesuita 5', 'Quinto jesuita', 'img5.jpg');
