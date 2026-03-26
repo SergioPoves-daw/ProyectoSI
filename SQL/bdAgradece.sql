4f2e88e3c6fd8f68dd8f260b7809ff7ea38ee35b
SergioPoves-daw <spovescamison.guadalupe@alumnado.fundacionloyola.net>
2026-03-26 16:48:14 +0100

    Consulta de nueva base de datos, e INSERT de alumnos con la nueva BD

---
+++ SQL/bdAgradece.sql
@@ -0,0 +1,24 @@
+USE `user1daw_BD1-20`;
+CREATE TABLE alumnos (
+	equipo char(2) NOT NULL PRIMARY KEY,
+	nombre varchar(40) NOT NULL,
+	usuario varchar(20) NOT NULL UNIQUE, -- Nombre con el que se inicia sesión. Sin espacios
+	password varchar(20) NOT NULL,
+	nombreJesuita varchar(100) NOT NULL,
+	infoJesuita varchar(250)NOT NULL,
+	web varchar(30) NOT NULL UNIQUE -- Nombre de la carpeta, que debe tener un archivo index.php, más el css y la imagen del jesuita.	
+);
+
+CREATE TABLE agradecimientos (
+    idAgradecimiento smallint unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	mensaje varchar(300) NOT NULL,
+	idEmisor char(2) NOT NULL,
+	idReceptor char(2) NOT NULL,
+	fecha_hora timestamp default NOW(),
+	CONSTRAINT CK_diferentes CHECK (idEmisor!=idReceptor ),
+	CONSTRAINT FK_emisor FOREIGN KEY (idEmisor) REFERENCES alumnos (equipo),
+	CONSTRAINT FK_receptor FOREIGN KEY (idReceptor) REFERENCES alumnos (equipo)
+
+);
+
+CREATE UNIQUE INDEX IX_agradecimientoUnico ON agradecimientos(idEmisor,idReceptor);
---
+++ SQL/insertAlumnosNuevo.sql
@@ -0,0 +1,6 @@
+INSERT INTO alumnos (equipo, nombre, usuario, password, nombreJesuita, infoJesuita, web) VALUES
+	('01', 'Manuel', 'Usuario 1', '1111', 'Jesuita 1', 'Primer Jesuita', 'web01'),
+	('02', 'Samuel', 'Usuario 2', '2222', 'Jesuita 2', 'Segundo Jesuita', 'web02'),
+	('03', 'Yehu', 'Usuario 3', '3333', 'Jesuita 3', 'Tercer Jesuita', 'web03'),
+	('04', 'Jose', 'Usuario 4', '4444', 'Jesuita 4', 'Cuarto Jesuita', 'web04'),
+	('05', 'Carlos', 'Usuario 5', '5555', 'Jesuita 5', 'Quinto Jesuita', 'web05');
